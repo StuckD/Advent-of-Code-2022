b2d605f9f1465a05fb0ba1e274227751fea2f120
stuckd <stuck.dominik@gmail.com>
2022-12-03 09:08:27 +0100

    solves day 3 part 2

--- days/day_3.py
+++ days/day_3.py
@@ -25,4 +25,23 @@ def solve(inputs):
                 if compartments[1].__contains__(item) and not item == old_item:
                     result+= priority_list.rfind(item)+1
                     old_item = item
-        return(result)
+
+        # part 2
+        groups = [[]]
+        current_group = 0
+        for i in inputs:
+            rucksack = i.replace("\n", "")
+            if len(groups[current_group]) == 3:
+                current_group+=1
+                groups.append([])
+            groups[current_group].append(rucksack)
+
+        total_sum = 0
+        for group in groups:
+            group_badge = ""
+            for badge in group[0]:
+                if group[1].__contains__(badge) and group[2].__contains__(badge) and badge != group_badge:
+                    group_badge = badge
+
+            total_sum+= priority_list.rfind(group_badge)+1
+        return([result, total_sum])
