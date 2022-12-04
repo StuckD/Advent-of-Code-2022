84886de0da840754dd00c02dd21ad797c0351557
stuckd <stuck.dominik@gmail.com>
2022-12-04 12:51:39 +0100

    removes unnecessary stuff

--- days/day_4.py
+++ days/day_4.py
@@ -1,9 +1,3 @@
-def areaToString(a):
-    s = ""
-    for c in a:
-        s += str(c) + ","
-    return(s)
-
 def solve(inputs):
     matches = 0
     for line in inputs:
@@ -17,26 +11,4 @@ def solve(inputs):
             matches += 1
         elif int(b[0]) >= int(a[0]) and int(b[1]) <= int(a[1]):
             matches += 1
-
-    ## Doesnt work :()
-    # overlaps = 0
-    # pairs = map(lambda x: x.split(","), inputs)
-    # pairs = map(lambda x: map(lambda y: y.replace("\n", ""), x), pairs)
-
-    # for p in pairs:
-    #     p = list(p)
-    #     old_pair_start = 0
-    #     for i in enumerate(p):
-    #         if i[0] == old_pair_start:
-    #             continue
-
-    #         old_pair_start+= 2
-    #         first_area = list(p)[i[0]-1].split("-")
-    #         second_area = list(p)[i[0]].split("-")
-
-    #         first_area_string = areaToString(list(range(int(first_area[0]), int(first_area[1])+1)))
-    #         second_area_string = areaToString(list(range(int(second_area[0]), int(second_area[1])+1)))
-
-    #         if first_area_string.__contains__(second_area_string) or second_area_string.__contains__(first_area_string):
-    #             overlaps+=1
     return(matches)
