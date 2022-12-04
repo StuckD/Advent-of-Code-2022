835d74af94d519616df6f8f46fc84869f663dc68
stuckd <stuck.dominik@gmail.com>
2022-12-04 12:59:54 +0100

    solves day 4 part 2

--- days/day_4.py
+++ days/day_4.py
@@ -11,4 +11,16 @@ def solve(inputs):
             matches += 1
         elif int(b[0]) >= int(a[0]) and int(b[1]) <= int(a[1]):
             matches += 1
-    return(matches)
+
+    #part 2
+    intersections = 0
+    for line in inputs:
+        line = line.replace('\n', '')
+        linesplit = line.split(',')
+        a = linesplit[0].split('-')
+        b = linesplit[1].split('-')
+        a = set(range(int(a[0]), int(a[1])+1))
+        b = set(range(int(b[0]), int(b[1])+1))
+        if a.intersection(b):
+            intersections+= 1
+    return([matches, intersections])
